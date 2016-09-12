eb7ade70c4d37e801bfa4b6b56287485c0c32643
Ramsey <rkarim@berkeley.edu>
2016-09-12 12:44:57 -0700

    week 2 work

--- Lab01/quickrun.py
+++ Lab01/quickrun.py
@@ -4,5 +4,10 @@ import unpack
 u = unpack.Unpack('PhotonCountDataE4/')
 s = stats.Stats(u)
 
-s.plot_sdom_by_rootn(0)
+#print u.file_names
 
+use = 5
+
+print u.file_names[use]
+
+s.plot_std_means()
--- Lab01/stats.py
+++ Lab01/stats.py
@@ -2,6 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+def histogram(dt, n, truth_test):
+    bw = float((dt.max() - dt.min())) / (n - 1.)
+    print "BW: ", bw
+    bin_l = np.arange(n) * bw + dt.min()
+    bin_count = np.array([])
+    bin_centers = np.array([])
+    for b in bin_l:
+        if truth_test(b):
+            continue
+        count = np.where((dt >= b) & (dt < b + bw))[0].size
+        bin_count = np.append(bin_count, count)
+        bin_centers = np.append(bin_centers, b + 0.5 * bw)
+    return bin_centers, bin_count
+
+
 class Stats:
     def __init__(self, unpacked):
         self.data = unpacked
@@ -29,6 +44,44 @@ class Stats:
             bins.append(j)
         return means, bins
 
+    def sdom(self, trial):
+        sdoms = []
+        chunks = np.arange(5, 400, 5)
+        for chunk_size in chunks:
+            means, bins = self.mean_chunks(trial, chunk_size)
+            mu = self.mean(trial)
+            means = np.array(means)
+            sd_mean = np.sqrt(np.sum((means - mu)**2.)/(np.float(means.size) - 1))
+            sdoms.append(sd_mean)
+        return sdoms, chunks
+
+    def histo(self, trial, n):
+        def peak_test(b):
+            if b < 3000:
+                return True
+            return False
+
+        def always_false(b):
+            return False
+
+        dt = self.data.intervals[trial]
+        dtp = dt[np.where(dt < 4000)]
+        return histogram(dt, n, peak_test)
+
+    def compare_brightnesses(self):
+        std_devs = np.array([])
+        sample_means = np.array([])
+        for run in self.data.intervals:
+            std_dev = np.std(run)
+            sample_mean = np.mean(run)
+            std_devs = np.append(std_devs, std_dev)
+            sample_means = np.append(sample_means, sample_mean)
+        return sample_means, std_devs
+
+    """
+    Plot procedures start here:
+    """
+
     def plot_mean_chunks(self, trial, chunk_size):
         means, bins = self.mean_chunks(trial, chunk_size)
         plt.plot(bins, means, 'o', color='#005555')
@@ -47,17 +100,6 @@ class Stats:
                   + self.data.file_names[trial])
         plt.show()
 
-    def sdom(self, trial):
-        sdoms = []
-        chunks = np.arange(5, 400, 5)
-        for chunk_size in chunks:
-            means, bins = self.mean_chunks(trial, chunk_size)
-            mu = self.mean(trial)
-            means = np.array(means)
-            sd_mean = np.sqrt(np.sum((means - mu)**2.)/(np.float(means.size) - 1))
-            sdoms.append(sd_mean)
-        return sdoms, chunks
-
     def plot_sdom_by_size(self, trial):
         sdoms, chunks = self.sdom(trial)
         plt.plot(chunks, sdoms, 'o', color='#009999')
@@ -75,4 +117,15 @@ class Stats:
         plt.ylabel("Standard deviation of the mean (ticks)")
         plt.title("SDOM versus $1/\sqrt{sample \, size}$ "
                   + self.data.file_names[trial])
+        plt.show()
+
+    def plot_histo(self, trial, n):
+        bin_centers, bin_counts = self.histo(trial, n)
+        plt.plot(bin_centers, bin_counts, drawstyle='steps-mid')
+        plt.yscale('log')
+        plt.show()
+
+    def plot_std_means(self):
+        means, stds = self.compare_brightnesses()
+        plt.plot(means, stds, '.')
         plt.show()
