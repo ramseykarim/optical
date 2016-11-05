48af922e47ae31a136db1dda3c21a17c5de8e824
Ramsey Lee Karim <rkarim@berkeley.edu>
2016-11-04 18:24:28 -0700

    tracking updated

--- Lab03/tracking.py
+++ Lab03/tracking.py
@@ -11,16 +11,33 @@ file_names = gsf.generate_names(HAT_SCI_PATH)
 data = gsf.fits_open(file_names[0])
 
 
+def median_subtract(science_frame):
+    median = np.median(science_frame)
+    return science_frame - median
+
+
 class Tracking:
     def __init__(self, sci_path=HAT_SCI_PATH, dark_path=HAT_DRK_PATH, band='v'):
         self.sci_path = sci_path
         self.dark_frame = gsf.process_dark(dark_path)
         self.response_map = gsf.fits_open(band.lower() + "_band_response_map.fts")
         self.data = False
+        self.file_names = gsf.generate_names(HAT_SCI_PATH)
 
-    def dark_subtraction(self, science_frame):
-        data_dark_sub = science_frame - self.dark_frame
-        data_dark_sub -= np.median(data_dark_sub)
-        return data_dark_sub
+    def dark_subtract(self, science_frame):
+        return science_frame - self.dark_frame
 
-    # add in flattening here
+    def flatten(self, science_frame):
+        return science_frame / self.response_map
+
+    def make_adjustments(self, science_frame):
+        return median_subtract(self.flatten(self.dark_subtract(science_frame)))
+
+    def find_science_star(self, science_frame):
+        plt.imshow(science_frame)
+        plt.show()
+
+
+t = Tracking()
+t.data = t.make_adjustments(t.file_names[0])
+t.find_science_star(t.data)
