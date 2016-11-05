49552bd22885f3de9225a0a4274435297034b085
Ramsey <rkarim@berkeley.edu>
2016-11-04 18:32:05 -0700

    more updating

--- Lab03/tracking.py
+++ Lab03/tracking.py
@@ -6,6 +6,7 @@ import filefunctions as gsf
 HAT_PATH = "/home/data/Planet_Transit/HAT-P-56/"
 HAT_SCI_PATH = HAT_PATH + "SI/"
 HAT_DRK_PATH = HAT_PATH + "darks120s/"
+RESPONSE_PATH = "ResponseMaps/"
 
 file_names = gsf.generate_names(HAT_SCI_PATH)
 data = gsf.fits_open(file_names[0])
@@ -20,9 +21,12 @@ class Tracking:
     def __init__(self, sci_path=HAT_SCI_PATH, dark_path=HAT_DRK_PATH, band='v'):
         self.sci_path = sci_path
         self.dark_frame = gsf.process_dark(dark_path)
-        self.response_map = gsf.fits_open(band.lower() + "_band_response_map.fts")
+        self.response_map = gsf.fits_open(RESPONSE_PATH + band.lower() + "_band_response_map.fts")
         self.data = False
         self.file_names = gsf.generate_names(HAT_SCI_PATH)
+
+    def file_grab(self, name):
+        return gsf.fits_open(name)
 
     def dark_subtract(self, science_frame):
         return science_frame - self.dark_frame
@@ -39,5 +43,5 @@ class Tracking:
 
 
 t = Tracking()
-t.data = t.make_adjustments(t.file_names[0])
+t.data = t.make_adjustments(t.file_grab(t.file_names[0]))
 t.find_science_star(t.data)
