63ac357c827ba6a46ea09f3b5a0f70a096209322
Ramsey Lee Karim <rkarim@berkeley.edu>
2016-11-04 19:25:08 -0700

    trying star box more

--- Lab03/tracking.py
+++ Lab03/tracking.py
@@ -38,10 +38,11 @@ class Tracking:
         return median_subtract(self.flatten(self.dark_subtract(science_frame)))
 
     def find_science_star(self, index, initial_x, initial_y,
-                          search_radius=50, fine_radius=50):
+                          search_radius=50, coarse_radius=20, fine_radius=10):
         science_frame = self.file_grab(index)
         science_frame = self.make_adjustments(science_frame)
-        find_centroid_in_range(science_frame, initial_x, initial_y, search_radius, fine_radius)
+        find_centroid_in_range(science_frame, initial_x, initial_y, search_radius,
+                               coarse_radius, fine_radius)
         row_sum = np.sum(science_frame, axis=1)
         print science_frame.shape
         col_sum = np.sum(science_frame, axis=0)
@@ -53,20 +54,23 @@ class Tracking:
 
 
 def find_centroid_in_range(science_frame, initial_x, initial_y,
-                           search_radius, fine_radius):
+                           search_radius, coarse_radius, fine_radius):
     lo_x, hi_x = initial_x - search_radius, initial_x + search_radius
     lo_y, hi_y = initial_y - search_radius, initial_y + search_radius
     search_box = science_frame[lo_x:hi_x, lo_y:hi_y]
     x_c, y_c = centroid_2d(search_box)
     x_c, y_c = x_c + lo_x, y_c + lo_y
     x_max, y_max = np.where(search_box == np.max(search_box))
-    lo_x_f, hi_x_f = x_max - fine_radius, x_max + fine_radius
-    lo_y_f, hi_y_f = y_max - fine_radius, y_max + fine_radius
+    lo_x_f, hi_x_f = x_max - coarse_radius, x_max + coarse_radius
+    lo_y_f, hi_y_f = y_max - coarse_radius, y_max + coarse_radius
     star_box = search_box[lo_x_f:hi_x_f, lo_y_f:hi_y_f]
+    x_c_f, y_c_f = centroid_2d(star_box)
+    lo_x_f, hi_x_f = x_c_f - fine_radius, x_c_f + fine_radius
+    lo_y_f, hi_y_f = y_c_f - fine_radius, y_c_f + fine_radius
+    star_box = search_box[lo_x_f:hi_x_f, lo_y_f:hi_y_f]
+    x_c_f, y_c_f = x_c_f + lo_x + lo_x_f, y_c_f + lo_y + lo_y_f
     plt.figure()
     plt.imshow(star_box)
-    x_c_f, y_c_f = centroid_2d(star_box)
-    x_c_f, y_c_f = x_c_f + lo_x + lo_x_f, y_c_f + lo_y + lo_y_f
     print x_c, y_c
     print x_c_f, y_c_f
 #    print "ROUGH CENTROID: ({0:f}, {1:f})".format(x_c, y_c)
