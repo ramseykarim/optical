4c4b1dbb0ce3938bc3da5bcb46bbda68a7337fa7
Ramsey Lee Karim <rkarim@berkeley.edu>
2016-11-05 15:56:18 -0700

    beginning aperture

---
+++ Lab03/star.py
@@ -0,0 +1,77 @@
+import numpy as np
+
+
+class Star:
+    def __init__(self, identifier):
+        self.identifier = identifier
+        self.frames = []
+        self.last_seen = (-1, -1)
+
+    def add_frame(self, x_y_img_tuple):
+        assert isinstance(x_y_img_tuple, tuple)
+        frame = Frame(x_y_img_tuple)
+        self.frames.append(frame)
+        self.last_seen = (frame.x_entire, frame.y_entire)
+
+    def get_aperture(self, index):
+        find_aperture(self.frames[index])
+
+
+def find_aperture(frame):
+    assert isinstance(frame, Frame)
+    find_background_noise(frame.image)
+
+
+def find_background_noise(image):
+    assert isinstance(image, np.ndarray)
+    dim_1 = image.shape[0]
+    dim_2 = image.shape[1]
+    print dim_1, dim_2
+    side_1 = image[:3, :].flatten()
+    side_2 = image[dim_1 - 4:, :].flatten()
+    side_3 = image[3:dim_1 - 4, :3].flatten()
+    side_4 = image[3:dim_1 - 4, dim_2 - 4:].flatten()
+    noise_array = np.concatenate([side_1, side_2, side_3, side_4])
+    print noise_array.shape
+
+
+def find_centroid_in_range(science_frame, (initial_x, initial_y),
+                           search_radius, coarse_radius, fine_radius):
+    x_c, y_c, x_c_1, y_c_1 = find_centroid_helper(science_frame, (initial_x, initial_y), search_radius)
+    x_c_f, y_c_f, x_c_1, y_c_1 = find_centroid_helper(science_frame, (x_c, y_c), coarse_radius)
+    star_box = image_partition(science_frame, (x_c_f, y_c_f), fine_radius)
+    return x_c_f, y_c_f, x_c_1, y_c_1, star_box
+
+
+def find_centroid_helper(frame, (initial_x, initial_y), radius):
+    search_box, lo_x, lo_y = image_partition(frame, (initial_x, initial_y), radius)
+    x_c, y_c = centroid_2d(search_box)
+    x_c_entire, y_c_entire = x_c + lo_x, y_c + lo_y
+    return x_c_entire, y_c_entire, x_c, y_c
+
+
+def image_partition(frame, (initial_x, initial_y), radius):
+    lo_x, hi_x = initial_x - radius, initial_x + radius
+    lo_y, hi_y = initial_y - radius, initial_y + radius
+    search_box = frame[lo_x:hi_x, lo_y:hi_y]
+    return search_box, lo_x, lo_y
+
+
+def centroid(y):
+    x = np.arange(len(y))
+    return np.sum(x * y) / np.sum(y)
+
+
+def centroid_2d(box):
+    x_centroid = centroid(np.sum(box, axis=1))
+    y_centroid = centroid(np.sum(box, axis=0))
+    return x_centroid, y_centroid
+
+
+class Frame:
+    def __init__(self, (x_entire, y_entire, x, y, image)):
+        self.x_entire = x_entire
+        self.y_entire = y_entire
+        self.x = x
+        self.y = y
+        self.image = image
--- Lab03/tracking.py
+++ Lab03/tracking.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import filefunctions as gsf
+import star
 
 
 HAT_PATH = "/home/data/Planet_Transit/HAT-P-56/"
@@ -17,12 +18,22 @@ def median_subtract(science_frame):
     return science_frame - median
 
 
+def find_star(science_frame,
+              (initial_x, initial_y),
+              search_radius=50, coarse_radius=20, fine_radius=15):
+    x_t, y_t, x, y, star_box = star.find_centroid_in_range(science_frame,
+                                                           (initial_x, initial_y),
+                                                           search_radius, coarse_radius,
+                                                           fine_radius)
+    return x, y, star_box
+
+
 class Tracking:
     def __init__(self, sci_path=HAT_SCI_PATH, dark_path=HAT_DRK_PATH, band='v'):
         self.sci_path = sci_path
         self.dark_frame = gsf.process_dark(dark_path)
         self.response_map = gsf.fits_open(RESPONSE_PATH + band.lower() + "_band_response_map.fts")
-        self.data = False
+        self.stars = {}
         self.file_names = gsf.generate_names(HAT_SCI_PATH)
 
     def file_grab(self, index):
@@ -37,68 +48,19 @@ class Tracking:
     def make_adjustments(self, science_frame):
         return median_subtract(self.flatten(self.dark_subtract(science_frame)))
 
-    def find_science_star(self, index, initial_x, initial_y,
-                          search_radius=50, coarse_radius=20, fine_radius=15):
+    def find_initial(self, identifier, science_frame,
+                     initial_x, initial_y):
+        star_instance = star.Star(identifier)
+        star_instance.add_frame(find_star(science_frame, (initial_x, initial_y)))
+        self.stars[identifier] = star_instance
+
+    def star_search_loop(self, index, initial_x, initial_y):
         science_frame = self.file_grab(index)
         science_frame = self.make_adjustments(science_frame)
-        find_centroid_in_range(science_frame, initial_x, initial_y, search_radius,
-                               coarse_radius, fine_radius)
-        row_sum = np.sum(science_frame, axis=1)
-        print science_frame.shape
-        col_sum = np.sum(science_frame, axis=0)
-        print "ROW", row_sum.shape
-        print "COL", col_sum.shape
-        plt.figure()
-        plt.imshow(science_frame)
-        plt.show()
-
-
-def find_centroid_in_range(science_frame, initial_x, initial_y,
-                           search_radius, coarse_radius, fine_radius):
-    search_box = image_partition(science_frame, initial_x, initial_y, search_radius)
-    x_c, y_c = find_centroid_helper(science_frame, initial_x, initial_y, search_radius)
-    x_max, y_max = np.where(science_frame == np.max(science_frame))
-    print "Maxes:"
-    print x_c, x_max
-    print y_c, y_max
-    print "-------"
-    print np.max(search_box)
-    print np.max(science_frame)
-    print "-------"
-    x_c_f, y_c_f = find_centroid_helper(science_frame, x_c, y_c, coarse_radius)
-    star_box = image_partition(science_frame, x_c_f, y_c_f, fine_radius)
-    plt.figure()
-    plt.imshow(star_box)
-    print x_c, y_c
-    print x_c_f, y_c_f
-#    print "ROUGH CENTROID: ({0:f}, {1:f})".format(x_c, y_c)
-#    print "FINER CENTROID: ({0:f}, {1:f})".format(x_c_f, y_c_f)
-
-
-def find_centroid_helper(frame, initial_x, initial_y, radius):
-    search_box, lo_x, lo_y = image_partition(frame, initial_x, initial_y, radius)
-    x_c, y_c = centroid_2d(search_box)
-    x_c, y_c = x_c + lo_x, y_c + lo_y
-    return x_c, y_c
-
-
-def image_partition(frame, initial_x, initial_y, radius):
-    lo_x, hi_x = initial_x - radius, initial_x + radius
-    lo_y, hi_y = initial_y - radius, initial_y + radius
-    search_box = frame[lo_x:hi_x, lo_y:hi_y]
-    return search_box, lo_x, lo_y
-
-
-def centroid(y):
-    x = np.arange(len(y))
-    return np.sum(x * y) / np.sum(y)
-
-
-def centroid_2d(box):
-    x_centroid = centroid(np.sum(box, axis=1))
-    y_centroid = centroid(np.sum(box, axis=0))
-    return x_centroid, y_centroid
+        self.find_initial('science', science_frame, initial_x, initial_y)
+        self.stars['science'].get_aperature(0)
+        # TODO write this function!
 
 
 t = Tracking()
-t.find_science_star(0, 842, 857)
+t.star_search_loop(0, 842, 857)
