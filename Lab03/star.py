fb717ae4a36c0dcc28f047c125c092b1af3aa2e0
Ramsey <rkarim@berkeley.edu>
2016-11-06 03:57:25 -0800

    killin!

--- Lab03/star.py
+++ Lab03/star.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
 
 class Star:
@@ -15,12 +16,74 @@ class Star:
         self.last_seen = (frame.x_entire, frame.y_entire)
 
     def get_aperture(self, index):
-        find_aperture(self.frames[index])
+        return find_aperture(self.frames[index])
+
+
+class Frame:
+    def __init__(self, (x_entire, y_entire, x, y, image)):
+        self.x_entire = x_entire
+        self.y_entire = y_entire
+        self.x = x
+        self.y = y
+        self.image = image
+        self.total_power = -99
+
+    def get_total_power(self, radius):
+        self.total_power = integrate_aperture(self, radius)
+
+"""
+APERTURE
+"""
+
+
+def integrate_aperture(frame, radius):
+    x, y = frame.x, frame.y
+    adu = 0
+    for i in range(int(x) - radius + 1, int(x) + radius - 1):
+        for j in range(int(y) - radius + 1, int(y) + radius - 1):
+            distance = np.round(np.sqrt((i - x) ** 2. + (j - y) ** 2.))
+            if distance < radius:
+                adu += frame.image[i, j]
+    return adu
 
 
 def find_aperture(frame):
     assert isinstance(frame, Frame)
-    find_background_noise(frame.image)
+    x, y = frame.x, frame.y
+    radius = frame.image.shape[0] / 2
+    adu_by_radius = np.zeros(radius)
+    pixels_by_radius = np.zeros(radius)
+    for i in range(int(x) - radius + 1, int(x) + radius - 1):
+        for j in range(int(y) - radius + 1, int(y) + radius - 1):
+            distance = np.round(np.sqrt((i - x)**2. + (j - y)**2.))
+            if distance < radius:
+                pixel_array = np.ones(radius - distance)
+                adu_array = pixel_array * frame.image[i, j]
+                zero_array = np.zeros(distance)
+                adu_by_radius += np.concatenate([zero_array, adu_array])
+                pixels_by_radius += np.concatenate([zero_array, pixel_array])
+    sigma_b = find_background_noise(frame.image)
+    n_sigma_b_sq = background_noise_sq(pixels_by_radius, sigma_b)
+    sigma_p_sq = pixel_noise_sq(adu_by_radius)
+    sigma_f = np.sqrt(sigma_p_sq + n_sigma_b_sq)
+    signal_noise = adu_by_radius / sigma_f
+    aperture = np.where(signal_noise == np.max(signal_noise))[0][0]
+    # print "DESIRED APERTURE:", aperture
+    # plt.figure()
+    # plt.plot(signal_noise)
+    # plt.plot([aperture, aperture], [np.min(signal_noise), np.max(signal_noise)])
+    # plt.show()
+    return aperture
+
+
+def pixel_noise_sq(adu_array):
+    n_photon = adu_array * 1.7 / 0.37
+    sigma_p = np.sqrt(n_photon) * 0.37 / 1.7
+    return sigma_p ** 2.
+
+
+def background_noise_sq(pixel_array, background_noise):
+    return pixel_array * (background_noise ** 2.)
 
 
 def find_background_noise(image):
@@ -33,6 +96,8 @@ def find_background_noise(image):
     side_3 = image[3:dim_1 - 4, :3].flatten()
     side_4 = image[3:dim_1 - 4, dim_2 - 4:].flatten()
     noise_array = np.concatenate([side_1, side_2, side_3, side_4])
+    noise = np.median(noise_array)
+    return noise
 
 
 def find_centroid_in_range(science_frame, (initial_x, initial_y),
@@ -40,7 +105,7 @@ def find_centroid_in_range(science_frame, (initial_x, initial_y),
     x_c, y_c, x_c_1, y_c_1 = find_centroid_helper(science_frame, (initial_x, initial_y), search_radius)
     x_c_f, y_c_f, x_c_1, y_c_1 = find_centroid_helper(science_frame, (x_c, y_c), coarse_radius)
     star_box, lo_x, lo_y = image_partition(science_frame, (x_c_f, y_c_f), fine_radius)
-    return x_c_f, y_c_f, x_c_1, y_c_1, star_box
+    return x_c_f, y_c_f, x_c_f - lo_x, y_c_f - lo_y, star_box
 
 
 def find_centroid_helper(frame, (initial_x, initial_y), radius):
@@ -59,21 +124,11 @@ def image_partition(frame, (initial_x, initial_y), radius):
 
 def centroid(y):
     x = np.arange(len(y))
-    return np.sum(x * y) / np.sum(y)
+    cent = np.sum(x * y) / np.sum(y)
+    return cent
 
 
 def centroid_2d(box):
     x_centroid = centroid(np.sum(box, axis=1))
     y_centroid = centroid(np.sum(box, axis=0))
     return x_centroid, y_centroid
-
-
-class Frame:
-    def __init__(self, (x_entire, y_entire, x, y, image)):
-        print "MEDIAN of IMG", np.median(image)
-
-        self.x_entire = x_entire
-        self.y_entire = y_entire
-        self.x = x
-        self.y = y
-        self.image = image
--- Lab03/tracking.py
+++ Lab03/tracking.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import filefunctions as gsf
 import star
+import gc
+import sys
 
 
 HAT_PATH = "/home/data/Planet_Transit/HAT-P-56/"
@@ -20,7 +22,7 @@ def median_subtract(science_frame):
 
 def find_star(science_frame,
               (initial_x, initial_y),
-              search_radius=50, coarse_radius=20, fine_radius=15):
+              search_radius=50, coarse_radius=20, fine_radius=50):
     x_t, y_t, x, y, star_box = star.find_centroid_in_range(science_frame,
                                                            (initial_x, initial_y),
                                                            search_radius, coarse_radius,
@@ -35,6 +37,7 @@ class Tracking:
         self.response_map = gsf.fits_open(RESPONSE_PATH + band.lower() + "_band_response_map.fts")
         self.stars = {}
         self.file_names = gsf.generate_names(HAT_SCI_PATH)
+        self.aperture = -1
 
     def file_grab(self, index):
         return gsf.fits_open(self.file_names[index])
@@ -49,19 +52,87 @@ class Tracking:
         return median_subtract(self.flatten(self.dark_subtract(science_frame)))
 
     def find_initial(self, identifier, science_frame,
-                     initial_x, initial_y):
+                     (initial_x, initial_y)):
         star_instance = star.Star(identifier)
         star_instance.add_frame(find_star(science_frame, (initial_x, initial_y)))
         self.stars[identifier] = star_instance
 
-    def star_search_loop(self, index, initial_x, initial_y):
-        science_frame = self.file_grab(index)
+    def find_again(self, identifier, science_frame):
+        star_instance = self.stars[identifier]
+        star_instance.add_frame(find_star(science_frame, star_instance.last_seen))
+
+    # def star_search_loop(self, identifier, initial_x, initial_y):
+    #     science_frame = self.file_grab(0)
+    #     science_frame = self.make_adjustments(science_frame)
+    #     self.find_initial(identifier, science_frame, (initial_x, initial_y))
+    #     for i in range(1, len(file_names)):
+    #         science_frame = self.file_grab(i)
+    #         science_frame = self.make_adjustments(science_frame)
+    #         self.find_again(identifier, science_frame)
+
+    def star_search_loop(self, id_dictionary):
+        science_frame = self.file_grab(0)
         science_frame = self.make_adjustments(science_frame)
-        print "SCIFRM MED", np.median(science_frame)
-        self.find_initial('science', science_frame, initial_x, initial_y)
-        self.stars['science'].get_aperture(0)
-        # TODO write this function!
+        for s in id_dictionary:
+            self.find_initial(s, science_frame, id_dictionary[s])
+        aperture = self.get_aperture(id_dictionary['science'])
+        print "Aperture set to", aperture
+        limit = len(self.file_names)
+        for i in range(1, limit):
+            sys.stdout.write(str(int(i*100./(limit - 1))) + " %")
+            sys.stdout.flush()
+            science_frame = self.file_grab(i)
+            science_frame = self.make_adjustments(science_frame)
+            [self.find_again(s, science_frame) for s in id_dictionary]
+            # if i % (limit / 10) == 0:
+            #     sys.stdout.write("Collecting garbage..")
+            #     sys.stdout.flush()
+            #     gc.collect()
+        print "Done!"
+
+    def get_aperture(self, (initial_x, initial_y)):
+        science_frame = self.file_grab(0)
+        science_frame = self.make_adjustments(science_frame)
+        self.find_initial('science', science_frame, (initial_x, initial_y))
+        return self.stars['science'].get_aperture(0)
+
+    def master_loop(self):
+        star_picks = {}
+        identifier = {'id': 0}
+
+        def onclick(event):
+            star_picks[identifier['id']] = (round(event.ydata), round(event.xdata))
+
+        first_frame = self.file_grab(0)
+        first_frame = self.make_adjustments(first_frame)
+        plt.ion()
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.imshow(first_frame, cmap='Greys')
+        cid = fig.canvas.mpl_connect('button_press_event', onclick)
+        print "Name a star and then click it. Enter 'd' when done."
+        print "PLEASE name at least one star 'science'!"
+        done = False
+        while not done:
+            a = raw_input("Name> ")
+            if a == 'd':
+                done = True
+                continue
+            identifier['id'] = a
+            print "Now click the star."
+            print "Name and select further stars."
+        fig.canvas.mpl_disconnect(cid)
+        plt.ioff()
+        plt.close()
+        print "You selected " + str(len(star_picks)) + " targets:"
+        for s in star_picks:
+            print s, star_picks[s]
+        raw_input("Ready?")
+        print "Beginning reduction"
+        self.star_search_loop(star_picks)
+
+
 
 
 t = Tracking()
-t.star_search_loop(0, 842, 857)
+t.master_loop()
